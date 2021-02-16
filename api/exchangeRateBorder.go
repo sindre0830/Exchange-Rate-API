72c90521f66016e41198327a8b49d5b24ad32334
Sindre Eiklid <sindre0830@gmail.com>
2021-02-17 00:55:37 +0100

    Added 'get' functionality for exchangeRateBorder and filtering

--- api/exchangeRateBorder.go
+++ api/exchangeRateBorder.go
@@ -1,9 +1,110 @@
 package api
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type exchangeRate struct {
+	Rates map[string]float32 `json:"rates"`
+	Base  string `json:"base"`
+	Date  string `json:"date"`
+}
+
+type exchangeRateBorder struct {
+	Rates map[string]countryCurrencyRate `json:"rates"`
+	Base string `json:"base"`
+}
+
+type countryCurrencyRate struct {
+	Currency string `json:"currency`
+	Rate float32 `json:"rate"`
+}
 
 func HandlerExchangeRateBorder(country string, limit int) {
-	//get bordering countries from country name
-	countryNeighbours := handlerCountryBorder(country, limit)
-	fmt.Println(countryNeighbours)
+	//get bordering countries from requested country
+	arrNeighbourCode := handlerCountryBorder(country, limit)
+	fmt.Println(arrNeighbourCode)
+	//get the currencies of the bordering countries
+	var arrNeighbourCurrency []string
+	for _, neighbour := range arrNeighbourCode {
+		arrNeighbourCurrency = append(arrNeighbourCurrency, handlerCountryCurrency(neighbour, true))
+	}
+	fmt.Println(arrNeighbourCurrency)
+	//get the currency of the requested country and set it as base
+	baseCurrency := handlerCountryCurrency(country, false)
+	//request all available currency data
+	var inpData exchangeRate
+	getExchangeRateBorderData(&inpData, baseCurrency)
+	//filter through the inputed data and generate data for output
+	var outData exchangeRateBorder
+	filterExchangeRateBorder(&inpData, &outData, arrNeighbourCode, arrNeighbourCurrency)
+		fmt.Println(outData)
+}
+
+func filterExchangeRateBorder(inpE *exchangeRate, outE *exchangeRateBorder, arrNeighbourCode []string, arrNeighbourCurrency []string) {
+	//initialize map in struct (could be done in a constructor)
+	outE.Rates = make(map[string]countryCurrencyRate)
+	//initialize a buffer struct to add in map
+	var bufferStruct countryCurrencyRate
+	//iterate through array of neighbours currency
+	for i, targetCurrency := range arrNeighbourCurrency {
+		//branch if target is the same as the base currency and set rate to 1
+		if targetCurrency == inpE.Base {
+			//update buffer
+			bufferStruct.Currency = inpE.Base
+			bufferStruct.Rate = 1
+			//update output
+			outE.Rates[arrNeighbourCode[i]] = bufferStruct
+		} else {
+			//iterate through map of all available currencies
+			for currencyName, currencyRate := range inpE.Rates {
+				//branch if target currency is available
+				if targetCurrency == currencyName {
+					//update buffer
+					bufferStruct.Currency = currencyName
+					bufferStruct.Rate = currencyRate
+					//update output
+					outE.Rates[arrNeighbourCode[i]] = bufferStruct
+				}
+			}
+		}
+	}
+	//update output
+	outE.Base = inpE.Base
+}
+
+func getExchangeRateBorderData(e *exchangeRate, baseCurrency string) {
+	url := "https://api.exchangeratesapi.io/latest?base=" + baseCurrency
+
+	// Create new request
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Errorf("Error in creating request:", err.Error())
+	}
+
+	apiClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	// Get response
+	res, err := apiClient.Do(req)
+	if err != nil {
+		fmt.Errorf("Error in response:", err.Error())
+	}
+
+	// Read output
+	output, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Errorf("Error when reading response: ", err.Error())
+	}
+
+	jsonErr := json.Unmarshal(output, &e)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
 }
