ccd52ad741a1d8e512f303ebd24e66a5d5d221b3
Sindre Eiklid <sindre0830@gmail.com>
2021-02-21 22:37:13 +0100

    Formatting

--- api/exchangeRateBorder.go
+++ api/exchangeRateBorder.go
@@ -37,41 +37,40 @@ func HandlerExchangeRateBorder(country string, limit int) ExchangeRateBorder {
 	//filter through the inputed data and generate data for output
 	var outData ExchangeRateBorder
 	filterExchangeRateBorder(&inpData, &outData, arrNeighbourCode, arrNeighbourCurrency, limit)
-	
 	return outData
 }
 
-func filterExchangeRateBorder(inpE *exchangeRate, outE *ExchangeRateBorder, arrNeighbourCode []string, arrNeighbourCurrency []string, limit int) {
+func filterExchangeRateBorder(inpData *exchangeRate, outData *ExchangeRateBorder, arrNeighbourCode []string, arrNeighbourCurrency []string, limit int) {
 	//update output
-	outE.Base = inpE.Base
+	outData.Base = inpData.Base
 	//initialize map in struct (could be done in a constructor)
-	outE.Rates = make(map[string]countryCurrencyRate)
+	outData.Rates = make(map[string]countryCurrencyRate)
 	//initialize a buffer struct to add in map
 	var bufferStruct countryCurrencyRate
 	//iterate through array of neighbours currency
 	for i, targetCurrency := range arrNeighbourCurrency {
 		//branch if target is the same as the base currency and set rate to 1
-		if targetCurrency == inpE.Base {
+		if targetCurrency == inpData.Base {
 			//update buffer
-			bufferStruct.Currency = inpE.Base
+			bufferStruct.Currency = inpData.Base
 			bufferStruct.Rate = 1
 			//update output
-			outE.Rates[arrNeighbourCode[i]] = bufferStruct
+			outData.Rates[arrNeighbourCode[i]] = bufferStruct
 			//check if limit is hit
-			if (limit > 0) && (len(outE.Rates) >= limit) {
+			if (limit > 0) && (len(outData.Rates) >= limit) {
 				return
 			}
 		} else {
 			//iterate through map of all available currencies
-			for currencyName, currencyRate := range inpE.Rates {
+			for currencyName, currencyRate := range inpData.Rates {
 				//branch if target currency is available
 				if targetCurrency == currencyName {
 					//update buffer
 					bufferStruct.Currency = currencyName
 					bufferStruct.Rate = currencyRate
 					//update output
-					outE.Rates[arrNeighbourCode[i]] = bufferStruct
-					if (limit > 0) && (len(outE.Rates) >= limit) {
+					outData.Rates[arrNeighbourCode[i]] = bufferStruct
+					if (limit > 0) && (len(outData.Rates) >= limit) {
 						return
 					}
 				}
