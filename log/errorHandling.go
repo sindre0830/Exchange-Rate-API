1bf83d351162d81aae74769ab150fb9916ea32ba
Sindre Eiklid <sindre0830@gmail.com>
2021-02-24 11:20:25 +0100

    Added error output to console

--- log/errorHandling.go
+++ log/errorHandling.go
@@ -23,13 +23,16 @@ func UpdateErrorMessage(status int, loc string, err string, reason string) {
 }
 
 func PrintErrorInformation(w http.ResponseWriter) {
-	//update header to json
+	//update header to json and set http code
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(ErrorMsg.StatusCode)
 	//send error output to user
 	err := json.NewEncoder(w).Encode(ErrorMsg)
 	//branch if something went wrong with output
 	if err != nil {
-		fmt.Println("ERROR encoding JSON", err)
+		fmt.Println("ERROR encoding JSON in PrintErrorInformation()", err)
+		return
 	}
+	//send error output to console
+	fmt.Printf("\nstatus_code: %v,\nlocation: %s,\nraw_error: %s,\npossible_reason: %s\n", ErrorMsg.StatusCode, ErrorMsg.Location, ErrorMsg.RawError, ErrorMsg.PossibleReason)
 }
