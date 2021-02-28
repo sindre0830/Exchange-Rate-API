63169aa3ce56e9fd49188d42b6411e6d35b3e266
Sindre Eiklid <sindre0830@gmail.com>
2021-02-28 10:56:04 +0100

    Added new dynamic error handling

--- api/dataHandeling.go
+++ api/dataHandeling.go
@@ -14,7 +14,7 @@ func requestData(url string) ([]byte, error) {
 	//create new request
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	//branch if there is an error
-	if err != nil {
+	if err != nil || req.Response.StatusCode != 200 {
 		debug.UpdateStatusCode(req.Response.StatusCode)
 		return nil, err
 	}
